9d19ded73ec9c251e4fdeb7f8dced6a2de915738
Romina Aguado <rominaaguado@Rominas-MacBook-Pro.local>
2021-02-10 22:26:37 -0600

    PUT working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,26 +21,22 @@ module.exports = (app) => {
       });
   });
 
-  app.put("/api/workouts/:workout", ({ params, body }, res) => {
-    console.log("hi", params.workout);
+  app.put("/api/workouts/:id", (req, res) => {
+    // app.put("/api/posts/:id", function(req, res) {
     db.Workout.findByIdAndUpdate(
-      params.workout.trim(),
-      { $push: { exercises: body } },
-      { new: true, upsert: true, useFindandModify: false }
-      //
-      //   (updateWorkout) => {
-      //     res.json(updatedWorkout);
-      //   }
-      //   (updateWorkout) => {
-      //     res.json(updatedWorkout);
-      //   }
+      { _id: req.params.id },
+      {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body },
+      },
+      // { exercises: req.body.exercises }
+      { new: true }
     )
-      .then((updateWorkout) => {
-        console.log(updateWorkout);
-        res.json(updateWorkout);
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
       })
       .catch((err) => {
-        console.log("OH NO", err);
+        res.json(err);
       });
   });
 
@@ -50,3 +46,29 @@ module.exports = (app) => {
     });
   });
 };
+
+// console.log("hi", params.workout);
+// db.Workout.findByIdAndUpdate(
+//   params.workout.trim(),
+//   { _id: params.id },
+//   { _id: params.workout },
+//   { $push: { exercises: body } },
+//   { new: true, upsert: true }
+//useFindandModify: false
+//   (updateWorkout) => {
+//     res.json(updatedWorkout);
+//   }
+//   (updateWorkout) => {
+//     res.json(updatedWorkout);
+//   }
+// )
+//   .then((updateWorkout) => {
+//     console.log(updateWorkout);
+//     res.json(updateWorkout);
+//   })
+//   .catch((err) => {
+//     console.log("OH NO", err);
+//   });
+// });
+
+// };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +18,7 @@ app.use(express.static("public"));
 //   useNewUrlParser: true,
 // });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
